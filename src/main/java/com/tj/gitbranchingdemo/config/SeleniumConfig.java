4684a396a0a88fbc0ebd4b293bd1c990030a7fda
Tim Johnston <tedwardj11@gmail.com>
2019-10-22 07:58:09 -0400

    implemented simple selenium test to check for correct home page header

---
+++ src/main/java/com/tj/gitbranchingdemo/config/SeleniumConfig.java
@@ -0,0 +1,36 @@
+package com.tj.gitbranchingdemo.config;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class SeleniumConfig {
+	
+	
+	private WebDriver driver;
+	
+	public SeleniumConfig() {
+		Capabilities capabilities = DesiredCapabilities.firefox();
+		driver = new FirefoxDriver(capabilities);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	}
+	
+	static {
+		System.setProperty("webdriver.gecko.driver", "C:\\projects\\env\\GeckoDriver\\geckodriver-v0.26.0-win64\\geckodriver.exe");
+	}
+
+	public WebDriver getDriver() {
+		return driver;
+	}
+
+	public void setDriver(WebDriver driver) {
+		this.driver = driver;
+	}
+	
+	
+	
+
+}
---
+++ src/test/java/com/tj/gitbranchingdemo/selenium/SeleniumTests.java
@@ -0,0 +1,28 @@
+package com.tj.gitbranchingdemo.selenium;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import com.tj.gitbranchingdemo.config.SeleniumConfig;
+
+public class SeleniumTests {
+	
+	@Test
+	public void getHeaderFromHomePage() {
+		
+		SeleniumConfig config = new SeleniumConfig();
+		WebDriver driver = config.getDriver();
+		
+		driver.get("http://localhost:8080/home");
+		
+		String expectedHeader = "Hello World";
+		String actualHeader = driver.findElement(By.id("header")).getText();
+		
+		assertTrue("Test failed: Incorrect header.", expectedHeader.equals(actualHeader));
+		
+	}
+
+}
